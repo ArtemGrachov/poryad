082b3b0070b997b93b1ea9881e37d383cfe32be7
Artem Grachov <grachov2195@gmail.com>
2017-11-08 14:08:13 +0200

    Slider. Slider nav. Poject slider

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
-
     const sliders = $('.slider');
-
     sliders.each(function () {
-
         const $this = $(this),
             slider = $this.find('.slider-list'),
             prevArrow = $this.find('.slider-toggle__prev'),
             nextArrow = $this.find('.slider-toggle__next');
-
         slider.slick({
             autoplay: true,
             autoplaySpeed: 1200,
