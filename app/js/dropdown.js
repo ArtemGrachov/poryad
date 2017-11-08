5ca0886b81e455cc422c92a9e5ea187b9bae62a2
Artem Grachov <grachov2195@gmail.com>
2017-11-08 17:31:55 +0200

    SCSS/JS Dropdown

--- app/js/dropdown.js
+++ app/js/dropdown.js
@@ -22,22 +22,3 @@ const dropdown = function () {
             }
         })
 }
-$(document).ready(function () {
-    slider();
-    dropdown();
-})
-const slider = function () {
-    const sliders = $('.slider');
-    sliders.each(function () {
-        const $this = $(this),
-            slider = $this.find('.slider-list'),
-            prevArrow = $this.find('.slider-toggle__prev'),
-            nextArrow = $this.find('.slider-toggle__next');
-        slider.slick({
-            autoplay: true,
-            autoplaySpeed: 1200,
-            prevArrow: prevArrow,
-            nextArrow: nextArrow
-        })
-    })
-}
---
+++ app/js/slider.js
@@ -0,0 +1,15 @@
+const slider = function () {
+    const sliders = $('.slider');
+    sliders.each(function () {
+        const $this = $(this),
+            slider = $this.find('.slider-list'),
+            prevArrow = $this.find('.slider-toggle__prev'),
+            nextArrow = $this.find('.slider-toggle__next');
+        slider.slick({
+            autoplay: true,
+            autoplaySpeed: 1200,
+            prevArrow: prevArrow,
+            nextArrow: nextArrow
+        })
+    })
+}
