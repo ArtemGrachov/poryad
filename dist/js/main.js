a7668544da2fb340a4ea29611e124d407168cdca
Artem Grachov <grachov2195@gmail.com>
2017-11-09 03:28:11 +0200

    HTML/CSS Slider-top: layer above slider; slides

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,15 +26,15 @@ $(document).ready(function () {
     slider();
     dropdown();
 })
-const slider = function () {
+const slider = function() {
     const sliders = $('.slider');
-    sliders.each(function () {
+    sliders.each(function() {
         const $this = $(this),
             slider = $this.find('.slider-list'),
             prevArrow = $this.find('.slider-toggle__prev'),
             nextArrow = $this.find('.slider-toggle__next');
         slider.slick({
-            autoplay: true,
+            // autoplay: true,
             autoplaySpeed: 1200,
             prevArrow: prevArrow,
             nextArrow: nextArrow
