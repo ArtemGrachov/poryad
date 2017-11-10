51186af86e9ca33ee41482de88c7a86c00e83c79
Artem Grachov <grachov2195@gmail.com>
2017-11-10 16:59:27 +0200

    SCSS: added letter-spacing in em to %title

--- app/js/positions.js
+++ app/js/positions.js
@@ -1,10 +1,17 @@
 const positions = function () {
+    $('.positions-nav__close').on('click', function () {
+        $(this)
+            .closest('.positions-item')
+            .removeClass('active');
+    })
     $('.positions-item').on('click', function () {
         const $this = $(this);
-        $this
-            .toggleClass('active');
-        $this
-            .siblings()
-            .removeClass('active');
+        if (!$this.hasClass('active')) {
+            $this
+                .toggleClass('active');
+            $this
+                .siblings()
+                .removeClass('active');
+        }
     })
 }
